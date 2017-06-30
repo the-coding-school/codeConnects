fdd0abfda2c5c6afb301db6933cc8473f924a514
Mindi Cao <mindip@users.noreply.github.com>
2017-06-30 16:21:35 -0700

    Initial commit

---
+++ routes/auth.js
@@ -0,0 +1,31 @@
+module.exports = function(app, passport) {
+
+    // =====================================
+    // LOGIN ===============================
+    // =====================================
+    // show the login form
+    app.get('/login', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('login.pug', { message: req.flash('loginMessage') });
+    });
+
+    // process the login form
+    // app.post('/login', do all our passport stuff here);
+
+    // =====================================
+    // SIGNUP ==============================
+    // =====================================
+    // show the signup form
+    app.get('/signup', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('signup.pug', { message: req.flash('signupMessage') });
+    });
+
+    // process the signup form
+    // app.post('/signup', do all our passport stuff here);
+
+};
+
+
