4a5945fba95bd663c6027a9bcf034546f53f76ae
pho-netik <oacidnim@g.ucla.edu>
2017-07-08 21:35:25 -0700

    Update README.md and updated package.json to handle nodemon dependency

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use(function(req, res, next){
 
 app.use('/', index);
 app.use('/users', users);
+var auth = require('./routes/auth')(app, passport);
 
 
 // catch 404 and forward to error handler
