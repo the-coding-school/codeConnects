d883a956f09f3d56959b5022fb0bcf2a079c0d47
pho-netik <oacidnim@g.ucla.edu>
2017-07-22 15:46:04 -0700

    correct redirects for student/teacher bios

--- routes/auth.js
+++ routes/auth.js
@@ -42,9 +42,16 @@ module.exports = function(app, passport) {
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('profile.pug', {
-            user : req.user.attrs // get the user out of session and pass to template
-        });
+        if(req.user.attrs.role == "student"){
+            res.render('student-bio.pug', {
+                user : req.user.attrs // get the user out of session and pass to template
+            });
+        }
+        if(req.user.attrs.role == "teacher"){
+             res.render('teacher-bio.pug', {
+                 user : req.user.attrs // get the user out of session and pass to template
+            });
+        }
     });
 
     // =====================================
