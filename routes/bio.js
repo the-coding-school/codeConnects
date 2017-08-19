39ee39b375349d8f4a2555b23e84d0d3b9e0fab9
Connor <csvrcek@umich.edu>
2017-08-19 16:02:28 -0400

    Created render_student and render_teacher functions.

--- routes/bio.js
+++ routes/bio.js
@@ -6,31 +6,17 @@ module.exports = function(app, passport) {
         // we will use route middleware to verify this (the isLoggedIn function)
         app.get('/profile', isLoggedIn, function(req, res) {
             var attributes = req.user.attrs;
+
             if(attributes.role == "student"){
-                 /* function render_student*/
-                if(attr.approved == true){
-                    res.render('student/student-bio', {
-                    user : req.user.attrs // get the user out of session and pass to template
-                });
-                }
-                else{
-                    res.redirect('/');
-                }
-                /*-----*/
+                
+                
             }
+            
             if(attributes.role == "teacher"){
-                /* function render_teacher*/
-                if(attributes.approved == true){
-                    res.render('teacher/teacher-bio', {
-                    user       : req.user.attrs // get the user out of session and pass to template
-                });
-                }
-                else{
-                    //redirect to index if they're not approved
-                    res.redirect('/');
-                }
-                /*-----*/
+                
             }
+            
+
         });
 
         app.post('/teacher-bio', isLoggedIn, function(req, res){
@@ -55,9 +41,34 @@ function isLoggedIn(req, res, next) {
 }
 
 function render_student(res, attr){
-    /*--TO-DO--*/
+    
+    /* function render_student*/
+        if(attributes.approved == true){
+            res.render('student/student-bio', {
+            user : req.user.attrs // get the user out of session and pass to template
+        });
+        }
+        else{
+            res.redirect('/');
+        }
+    /*-----*/
+    
+    
 }
 
 function render_teacher(res, attr){
-    /*--TO-DO--*/
+    
+    /* function render_teacher*/
+        if(attributes.approved == true){
+            res.render('teacher/teacher-bio', {
+            user       : req.user.attrs // get the user out of session and pass to template
+        });
+        }
+        else{
+            //redirect to index if they're not approved
+            res.redirect('/');
+        }
+        /*-----*/
+    
+    
 }
