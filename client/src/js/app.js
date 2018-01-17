99df4196212fc9e73129bb6cc948c594efb86d14
Brendon Daugherty <brendon1097@gmail.com>
2018-01-17 12:27:32 -0800

    Update app.js

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Header from './components/Header';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
-	    <Header/>
+	<Header/>
+
+	<BrowserRouter>
+	    <Navbar />
+      		<Switch>
+	    	</Switch>
+	</BrowserRouter>
+	<Footer />
+
       </div>
     );
   }
