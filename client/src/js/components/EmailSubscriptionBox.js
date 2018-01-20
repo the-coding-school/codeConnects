e97ffb654953b03cca4801d9c30677d7e22d52fd
Nandun <nandun.gunawardhana@gmail.com>
2018-01-20 17:35:16 -0500

    Update EmailSubscriptionBox.js
    updated html for EmailSubscriptionBox

--- client/src/js/components/EmailSubscriptionBox.js
+++ client/src/js/components/EmailSubscriptionBox.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
-export default class EmailSubscriptionBox extends React.Component{
-  render()
-    {return (
-    <div>
-    </div>
+
+class EmailSubscriptionBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'Enter Your Email'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Keep up to date with our latest news, events and calendar! Subscribe to our Newsletter.
+
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
+
 }
+
+export default EmailSubscriptionBox;
